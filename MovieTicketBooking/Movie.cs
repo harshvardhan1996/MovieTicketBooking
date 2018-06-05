85940b69a924162db56ae42ba63b9bff6fbd4c0a
harshvardhan1996 <harshamazon1996@gmail.com>
2018-06-05 13:34:19 +0530

    Added Movie , Screen and Theatre Class

---
+++ MovieTicketBooking/Movie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTicketBooking
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string MovieName { get; set; }
+        public string Director { get; set; }
+        public string Producer { get; set; }
+        public string Cast { get; set; }
+        public double Duration { get; set; }
+        public string Story { get; set; }
+        public string Type { get; set; }
+
+        public Movie(string MovieName, string Director, string Producer, string Cast, double Duration, string Story, string Type)
+        {
+            Random random = new Random();
+            this.MovieID = random.Next();
+            this.MovieName = MovieName;
+            this.Director = Director;
+            this.Producer = Producer;
+            this.Cast = Cast;
+            this.Duration = Duration;
+            this.Story = Story;
+            this.Type = Type;
+        }
+
+        public void DisplayMovieDetails()
+        {
+            Console.WriteLine("Movie Id is : {0}", MovieID);
+            Console.WriteLine("Movie Name is : {0}", MovieName);
+            Console.WriteLine("Director is : {0}", Director);
+            Console.WriteLine("Porducer is : {0}", Producer);
+            Console.WriteLine("Cast is : {0}", Cast);
+            Console.WriteLine("Duration is : {0}", Duration);
+            Console.WriteLine("Story is : {0}", Story);
+            Console.WriteLine("Type is : {0}", Type);
+        }
+
+
+
+
+
+    }
+}
---
+++ MovieTicketBooking/Theatre.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTicketBooking
+{
+    public class Theatre
+    {
+        public int TheatreID { get; set; }
+        public string TheatreName { get; set; }
+        public string CityName { get; set; }
+        public string Address { get; set; }
+        public int NumberOfScreens { get; set; }
+        public List<Screen> Screens { get; set; }
+
+        public Theatre(string TheatreName, string CityName, string Address, int NumberOfScreens)
+        {
+            Random ran = new Random();
+            this.TheatreID = ran.Next();
+            this.TheatreName = TheatreName;
+            this.CityName = CityName;
+            this.Address = Address;
+            this.NumberOfScreens = NumberOfScreens;
+            this.Screens = new List<Screen>(NumberOfScreens);
+        }
+
+        public void DisplayTheatreDetails()
+        {
+            Console.WriteLine("Threatre ID is: {0}", TheatreID);
+            Console.WriteLine("Threatre Name is: {0}", TheatreName);
+            Console.WriteLine("CityName is: {0}", CityName);
+            Console.WriteLine("Address is: {0}", Address);
+            Console.WriteLine("NumberOfScreens is: {0}", NumberOfScreens);
+            Console.WriteLine("Screens Information:");
+
+            foreach (Screen sc in Screens)
+            {
+                Console.WriteLine(sc);
+            }
+        }
+    }
+}
