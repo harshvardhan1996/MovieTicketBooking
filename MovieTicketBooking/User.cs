97267cdf1d792109b25c5c5a96bc0d939ec6ef38
harshvardhan1996 <harshamazon1996@gmail.com>
2018-06-05 15:17:30 +0530

    Created and Updated User Class

---
+++ MovieTicketBooking/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTicketBooking
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+
+        public User(string UserName, string Password, string UserType)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.UserType = UserType;
+        }
+    }
+}
